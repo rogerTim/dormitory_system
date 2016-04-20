d4516f914a5e1cfe7e69a08d47deb68daa58b26b
GAOJUNHUI <591195690@qq.com>
2016-04-20 20:35:20 +0800

    完整接口

--- dormitory/views.py
+++ dormitory/views.py
@@ -1,10 +1,39 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from dormitory.models import User_answer, DorArrange, Dormitory, User, Question
+from dormitory.models import User_answer, DorArrange, Dormitory, User, Question, Building
 from django.http import HttpResponse, JsonResponse
+import json
+from datetime import datetime
+
+
 # Create your views here.
 
 
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into json. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = json.dumps(objects, default=json_serial)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = json.dumps(str(objects))
+        return HttpResponse(data, "application/json")
+
+    return decorator
+
+
+@json_response
 def student_login(request):
     """ 学生登录接口
         方式:
@@ -21,13 +50,21 @@ def student_login(request):
     try:
         if pwd == User.objects.get(id=schId).pwd:
             name = User.objects.get(id=schId).name
-            return JsonResponse({'status': 1, 'username': name})
+            print 'ok'
+            # return JsonResponse({'status': 1, 'username': name})
+            return {'status': 1, 'username': name}
         else:
-            return JsonResponse({'status': 0})
-    except:
-        return JsonResponse({'status': 0})
+            print pwd, User.objects.get(id=schId).pwd
+            print 'nononono'
+            return {'status': 0}
+            # return JsonResponse({'status': 0})
+    except Exception as ex:
+        print ex
+        return {'status': 0}
+        # return JsonResponse({'status': 0})
 
 
+@json_response
 def get_question(request):
     """ 问卷题目
         方式:
@@ -40,17 +77,15 @@ def get_question(request):
     data = []
     # 将问卷内容全部导出并存在text_dict中
     for each in Question.objects.all():
-        question = {}
-        question['id'] = each.id
-        question['question'] = each.content
-        question['options'] = []
+        question = {'id': each.id, 'question': each.content, 'options': []}
         for option in each.option.split('||'):
             option.replace('$', '.')
             question['options'].append(option)
         data.append(question)
-    return JsonResponse({'status': 1, 'data': data})
+    return {'status': 1, 'data': data}
 
 
+@json_response
 def save_answer(request):
     """ 问卷提交接口
         方式:
@@ -69,11 +104,12 @@ def save_answer(request):
             ans += u'%s$%s||' % (answer['id'], answer['answer'])
         ans.rstrip('||')
         User_answer.object.create(user_id=schID, answer=ans)
-        return JsonResponse({'status': 1})
+        return {'status': 1}
     else:
-        return JsonResponse({'status': 0})
+        return {'status': 0}
 
 
+@json_response
 def get_result(request):
     """ 学生查看宿舍分配结果接口
         方式:
@@ -86,32 +122,30 @@ def get_result(request):
                    roomates: [{schId: 学号, name: 姓名, major: 专业, hometown: 生源地}, ...]
             }
     """
-    if request.GET['schId'] and DorArrange.objects.get('schId'):
-        # 学生基本信息
-        user_id = request.GET.get('schId')
-        dor_id = DorArrange.objects.get(user_id=user_id)
+    try:
+        if request.GET['schId'] and DorArrange.objects.get(user_id=request.GET['schId']):
+            # 学生基本信息
+            user_id = request.GET.get('schId')
+            dor_id = DorArrange.objects.get(user_id=user_id)
 
-        # 获取所在宿舍
-        dor = Dormitory.objects.get(dor_id=dor_id)
+            # 获取所在宿舍
+            dor = Dormitory.objects.get(dor_id=dor_id)
 
-        data = {}
-        data['dormitory'] = dor.name
-        # 获取室友信息
-        data['roomates'] = []
-        roomates = DorArrange.objects.filter(dor_id=dor_id).exclude(user_id=user_id)
-        for mate in roomates:
-            mate_id = mate.user_id
-            mate = User.objects.get(id=mate_id)
-            mate_dict = {}
-            mate_dict['schId'] = mate_id
-            mate_dict['name'] = mate.name
-            mate_dict['major'] = mate.major
-            mate_dict['hometown'] = mate.source
-            data['roomates'].append(mate_dict)
-        return JsonResponse({'status': 1, 'data': data})
-    return JsonResponse({'status': 1})
+            data = {'dormitory': dor.name, 'roomates': []}
+            # 获取室友信息
+            roomates = DorArrange.objects.filter(dor_id=dor_id).exclude(user_id=user_id)
+            for mate in roomates:
+                mate_id = mate.user_id
+                mate = User.objects.get(id=mate_id)
+                mate_dict = {'schId': mate_id, 'name': mate.name, 'major': mate.major, 'hometown': mate.source}
+                data['roomates'].append(mate_dict)
+            return {'status': 1, 'data': data}
+    except Exception as ex:
+        print ex
+        return {'status': 0}
 
 
+@json_response
 def admin_login(request):
     """ 管理员登录接口
         方式:
@@ -124,15 +158,16 @@ def admin_login(request):
             username: 用户名 失败则不返回
     """
     if request.POST.get('adminId') and request.POST.get('pwd'):
-        admin = User.objects.get(id=request.POST['adminId'])
-        if admin and request.POST['pwd'] == admin.pwd and admin.authority:
-            return JsonResponse({'status': 1, 'username': admin.name})
+        admin = User.objects.get(id=request.POST.get('adminId'))
+        if admin and request.POST.get('pwd') == admin.pwd and admin.authority:
+            return {'status': 1, 'username': admin.name}
         else:
-            return JsonResponse({'status': 0})
+            return {'status': 0}
     else:
-        return JsonResponse({'status': 0})
+        return {'status': 0}
 
 
+@json_response
 def percentage(request):
     """ 获取男生填写率和女生填写率(精度为0.1)
         方法:
@@ -155,7 +190,7 @@ def percentage(request):
     women_ans = 0
 
     # 统计数量
-    for each in User.objects.all():
+    for each in Users:
         if each.sex == 1:
             man_sum += 1
             if User_answers.get(user_id=each.id):
@@ -166,11 +201,12 @@ def percentage(request):
                 women_ans += 1
 
     # 计算男生填写率和女生填写率
-    man_per = int('%.2f' % (1.0*man_ans/man_sum))
-    women_per = int('%.2f' % (1.0*women_ans/women_sum))
-    return JsonResponse({'status': 1, 'data': {'man': man_per, 'women': women_per}})
+    man_per = int('%.2f' % (1.0 * man_ans / man_sum))
+    women_per = int('%.2f' % (1.0 * women_ans / women_sum))
+    return {'status': 1, 'data': {'man': man_per, 'women': women_per}}
 
 
+@json_response
 def output_all(request):
     """ 管理员界面导出全部宿舍的接口
         方式:
@@ -182,21 +218,21 @@ def output_all(request):
                  ]
     """
     data = []
-    dormitorys = Dormitory.objects
+    dormitories = Dormitory.objects.all()
     users = User.objects
-    for dormitory in dormitorys:
-        room = {}
-        room['id'] = dormitory.get('dor_id')
-        room['dormitory'] = dormitory.get('name')
-        room['roommates'] = []
-        for each in [each.user_id for each in DorArrange.objects.fliter(dor_id=room['id'])]:
-             roommate = users.get(id=each)
-             roommate_dict = {}
-             roommate_dict['schId'] = each
-             roommate_dict['name'] = roommate.name
-             roommate_dict['major'] = roommate.major
-             roommate_dict['hometown'] = roommate.source
-             room['roommates'].append(roommate)
+    for dormitory in dormitories:
+        room = {'id': dormitory.dor_id, 'dormitory': dormitory.buil_id.name+u'栋'+dormitory.name, 'roommates': []}
+        for each in [each.user_id for each in DorArrange.objects.filter(dor_id=room['id'])]:
+            roommate = users.get(id=each)
+            roommate_dict = {'schId': each, 'name': roommate.name, 'major': roommate.major, 'hometown': roommate.source}
+            room['roommates'].append(roommate_dict)
         data.append(room)
-    return JsonResponse({'data': data})
+    return data
 
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial[:10]
